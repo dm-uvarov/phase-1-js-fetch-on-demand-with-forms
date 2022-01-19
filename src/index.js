fb3c47e6391576cdba470b665c4f2fc79a836f23
Jimmy <dm.a.uvarov@gmail.com>
2022-01-19 16:56:15 -0500

    commit tree fb3b066e63bf7c2b164fdbb99f6b031c22c6b61c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
+
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const inputField = document.querySelector('input#searchByID');
+        console.log(inputField.value);
+
+        const fetchURL = `http://localhost:3000/movies/${inputField.value}`;
+
+        rs = fetch(fetchURL)
+            .then(r => r.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+
+                // console.log(data);
+            });
+
+
+
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
